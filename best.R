adfc59005d9bc60a1f277bd15a117b44d6a684fa
Mehieddine44 <momoh.boudissa@gmail.com>
2018-01-26 16:07:43 +0100

    Update best.R

--- best.R
+++ best.R
@@ -1,20 +1,20 @@
 best <- function(state, outcome) {
-        outcome_data <- read.csv("C:/Users/hp/Desktop/Career/My working directory/outcome-of-care-measures.csv")
+        # reading the data file using read.csv() function.
+        outcome_data <- read.csv("C:/Users/hp/Desktop/Career/My working directory/outcome-of-care-measures.csv", colClasses = "character")
         
         ## Check that state and outcome are valid
         sum = sum(as.character(outcome_data$State) == as.character(state))
         if(sum == 0) return("invalid state name")
-        outcomename = paste("Hospital.30.Day.Death..Mortality..Rates.from.",
-                            outcome, sep = "")
-        sum2 = sum(colnames(outcome_data)== outcomename)
-        if(sum2 == 0) return(sum2)
+        
+        outcome_index = 11*(outcome == "heart attack") + 17*(outcome == "heart failure") + 23*(outcome == "pneumonia")
+        if(outcome_index == 0) stop("invalid outcome")
         
         
        
          ## Return hospital name in that state with lowest 30-day death
          ## rate
-        index <- which.min(outcome_data[outcome_data$State== state,
-                                        colnames(outcome_data)==outcomename])
-        outcome_data[outcome_data$State== state,][index,]$Hospital.Name
+        index_of_best <- which.min(outcome_data[outcome_data$State== state,
+                                        outcome_index])
+        outcome_data[outcome_data$State== state,][index_of_best,]$Hospital.Name
         
         }
